92f3676f0d0df1caac06c820a75274936f3a872a
NGHANYISALAMATOUWOUYUEKE <www.nghanyisalamatou@gmail.com>
2024-07-30 16:55:55 +0100

    initial commit

---
+++ Tracker.js
@@ -0,0 +1,33 @@
+// const date = document.getElementById('date');
+// const detail = document.getElementById('detail');
+// const amount = document.getElementById('amount');
+// const button = document.getElementById('button');
+// const view = document.getElementById('view');
+
+// function addTask (){
+//     const task = [date.value, detail.value, amount.value];
+
+//     if (task) {
+//         amount.value = '';
+//         detail.value = '';
+//         date.value = '';
+//     }
+//     else{
+//         alert('please fill all the boxes')
+//     }
+//     display(task);
+// }
+// button.addEventListener('click' , addTask);
+
+// function createTaskElement(task) {
+//     const listItems = document.createElement('li') ;
+//     for(let i = 0; i<= task.length; i++){
+//         listItems += task.value;
+//     }
+//     view.appendChild(listItems);
+// }
+
+
+function submit(){
+    
+}
